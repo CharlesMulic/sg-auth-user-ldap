7ef5590f6008bc988351017b7ae7fb797738c724
Charlie <charles.mulic@gmail.com>
2018-12-30 15:48:48 -0600

    initial

---
+++ src/main/java/com/charliemulic/sg/authuserwithldap/AuthUserWithLdapApplication.java
@@ -0,0 +1,14 @@
+package com.charliemulic.sg.authuserwithldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserWithLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUserWithLdapApplication.class, args);
+    }
+
+}
+
